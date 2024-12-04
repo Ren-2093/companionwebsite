e1afea4ed236fafc34b52b3bcaf882560ad49dff
Ren-2093 <18Butler-EalesJ@hccmac.co.uk>
2024-12-04 09:28:40 +0000

    Update groups.js

--- routes/groups.js
+++ routes/groups.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateUser } = require('../middleware/auth');
+const Group = require('../models/Group');
 
+router.delete('/api/groups/:id', authenticateUser, async (req, res) => {
+    // Endpoint logic here
+});
+
+module.exports = router;
