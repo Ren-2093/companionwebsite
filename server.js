ceafc84c5d03654e6356fa5dc1286f326b458ce5
Ren-2093 <18Butler-EalesJ@hccmac.co.uk>
2024-12-04 10:47:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
-const deleteListing = require('./deletelisting'); // Correct path to the file
 const router = express.Router();
 
 // Initialize the app
